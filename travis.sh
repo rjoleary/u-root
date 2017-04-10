e44d6558a11e4c4b693247670e1b1bbf1e4ff800
Ryan O'Leary <ryanoleary@google.com>
2017-04-10 15:48:38 -0700

    WIP

--- travis.sh
+++ travis.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
-export GOPATH=/home/travis/gopath
 set -e
- (cd bb && go build . && ./bb)
- which go
- (cd scripts && go run ramfs.go -d -tmpdir=/tmp/u-root -removedir=false)
- GOBIN=/tmp/u-root/ubin GOROOT=/tmp/u-root/go GOPATH=/tmp/u-root CGO_ENABLED=0 /tmp/u-root/go/bin/go build -x github.com/u-root/u-root/cmds/ip
- (cd cmds && CGO_ENABLED=0 go build -a -installsuffix cgo -ldflags '-s' ./...)
- ls -l cmds/*
- (cd cmds && CGO_ENABLED=0 go test -a -installsuffix cgo -ldflags '-s' ./...)
- (cd cmds && CGO_ENABLED=0 go test -cover ./...)
- go tool vet cmds uroot netlink memmap
- go tool vet scripts/ramfs.go
- sudo date
- echo "Did it blend"
+export GOPATH=/home/travis/gopath
+export CGO_ENABLED=0
+GOFLAGS="-a -ldflags='-s'"
+
+which go
+ls -l cmds/*
+
+(cd bb && go build $GOFLAGS && du -h bb && ./bb)
+(cd scripts && go run ramfs.go -d -tmpdir=/tmp/u-root -removedir=false)
+(cd cmds && go build $GOFLAGS ./...)
+(cd cmds && go test $GOFLAGS ./...)
+(cd cmds && go test $GOFLAGS -cover ./...)
+(cd cmds && go test $GOFLAGS -race ./...)
+
+go tool vet cmds uroot netlink memmap
+go tool vet scripts/ramfs.go
+sudo date
+echo "Did it blend"
+
+# TODO: why?
+GOBIN=/tmp/u-root/ubin GOROOT=/tmp/u-root/go GOPATH=/tmp/u-root /tmp/u-root/go/bin/go build -x github.com/u-root/u-root/cmds/ip
